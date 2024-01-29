9080be2975a2c04dfabf69ae1ddecb7e0f7b7384
Guillaume PANNETIER <pannetierguillaume@gmail.com>
2024-01-29 11:37:25 +0100

    DES-1765: first test with Jira API

---
+++ scripts/jiratool.ts
@@ -0,0 +1,113 @@
+const { program } = require("commander");
+const fetch = require("node-fetch");
+
+interface autoconsentBrokenTicketArgs {
+  email: string;
+  token: string;
+  type: string;
+  summary: string;
+  description: string;
+  brokenSites: string[];
+}
+
+function createAutoconsentBrokenTicket({
+  email,
+  token,
+  type,
+  summary,
+  description,
+  brokenSites,
+}: autoconsentBrokenTicketArgs) {
+  const url = `http://localhost:8080/rest/api/3/issue/create`;
+
+  const body = {
+    fields: {
+      assignee: {
+        id: 123,
+      },
+      issuetype: {
+        id: "10000",
+      },
+      description: {
+        content: [
+          {
+            content: [
+              {
+                text: "Order entry fails when selecting supplier.",
+                type: "text",
+              },
+            ],
+            type: "paragraph",
+          },
+        ],
+        type: "doc",
+        version: 1,
+      },
+    },
+  };
+
+  const request = {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${Buffer.from(`${email}:${token}`).toString(
+        "base64"
+      )}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  };
+
+  const response = await fetch(
+    `https://tempest-tech.atlassian.net/rest/api/3/issue/${id}/${api}`,
+    request
+  );
+  const responseText = (await response.text()) || "{}";
+  const status = `${response.status} ${response.statusText}: ${responseText}`;
+
+  if (response.ok) {
+    return JSON.parse(responseText);
+  }
+
+  throw new Error(status);
+}
+
+async function queryIssue(api: string, { id, email, token }: { id: string; email: string; token: string }) {
+  const request = {
+    method: 'GET',
+    headers: {
+      Authorization: `Basic ${Buffer.from(`${email}:${token}`).toString('base64')}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const response = await fetch(`https://tempest-tech.atlassian.net/rest/api/3/issue/${id}`, request);
+  const responseText = (await response.text()) || '{}';
+  const status = `${response.status} ${response.statusText}: ${responseText}`;
+
+  if (response.ok) {
+    return JSON.parse(responseText);
+  }
+
+  throw new Error(status);
+}
+
+program
+  .command('query')
+  .requireOption('-i', '--id <ticket_id>')
+  .requiredOption('-e', '--email <user_email>', "user email")
+  .requiredOption('-t', '--token <user_token>', "user token")
+  .action(queryIssue);
+
+program
+  .command("create-ticket")
+  .requiredOption("-e", "--email <user_email>", "user email")
+  .requiredOption("-t", "--token <user_token>", "user_token")
+  .requiredOption("-i", "--issueType <issue_type>", "issue type")
+  .requiredOption("-s", "--summary <ticket summary>", "Ticket summary")
+  .requiredOption("-d", "--description <ticket summary>", "Ticket summary")
+  .requiredOption("-b", "--body <ticket body>", "Ticket body")
+  .requiredOption("-p", "--parent")
+  .option("-f", "--fix-version <fix_version>", "Fix version number")
+  .action(createAutoconsentBrokenTicket);
