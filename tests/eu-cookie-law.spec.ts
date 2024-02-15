827f98588125e3969b5fb0c467ca2149a21add6d
Guillaume PANNETIER <pannetierguillaume@gmail.com>
2024-02-15 10:28:21 +0100

    DES-1855: add snowplow.io to tests

--- tests/eu-cookie-law.spec.ts
+++ tests/eu-cookie-law.spec.ts
@@ -3,4 +3,5 @@ import generateCMPTests from "../playwright/runner";
 generateCMPTests('EU Cookie Law', [
   'https://pv-magazine-usa.com/',
   'https://www.prolificliving.com/',
+  'https://snowplow.io'
 ], {});
