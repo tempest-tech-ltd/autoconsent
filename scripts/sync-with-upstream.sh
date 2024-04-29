97f0ae001ab2d0da4eb780ace274703d1b32a5ce
Guillaume PANNETIER <pannetierguillaume@gmail.com>
2024-04-29 15:25:58 +0200

    sync: delete previous "upstream-sync" branch

--- scripts/sync-with-upstream.sh
+++ scripts/sync-with-upstream.sh
@@ -1,1 +1,2 @@
+git branch -D upstream-sync
 git checkout new-main && git checkout -b upstream-sync && git reset --hard upstream/main && git pull upstream main && git rebase new-main && git push --set-upstream origin upstream-sync
